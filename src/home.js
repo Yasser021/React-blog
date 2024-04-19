711a6fc20f7e35b5e3839f3063a05d3e83164269
Yasser <azisalfalahyasser@gmail.com>
2024-04-19 23:25:42 +0700

    add : "card blogs"

--- src/home.js
+++ src/home.js
@@ -1,15 +1,36 @@
+import { useState } from "react";
+
 const Home = () => {
-  const handleClick = () => {
-    console.log("Hello, There!");
-  };
+  const [blogs, setBlogs] = useState([
+    {
+      title: "This My First Blog",
+      body: "Hello There I learn React js Today",
+      author: "Yasser",
+      id: 1,
+    },
+    { title: "React js is Fun", body: "No it is not", author: "Yasser", id: 2 },
+    {
+      title: "Javascript is Fun",
+      body: "No it is not",
+      author: "Yasser",
+      id: 3,
+    },
+  ]);
   return (
     <div className="home">
       <div className="header">
         <h2>Home Page</h2>
-        <button onClick={handleClick} className="button">
-          See more !
+        <button className="button">
+          New Blog
         </button>
       </div>
+      {blogs.map(blog => (
+        <div className="blog-preview" key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>Writen by {blog.author}</p>
+            <p>{blog.body}</p>
+        </div>
+      ))}
     </div>
   );
 };
