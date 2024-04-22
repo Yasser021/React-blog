e2083ab6b01f8a582185ba74188d237fc2c098ae
Yasser <azisalfalahyasser@gmail.com>
2024-04-22 22:12:46 +0700

    chore : "handling error and conditional loading"

--- src/home.js
+++ src/home.js
@@ -3,20 +3,34 @@ import BlogList from "./blogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  // const handleDelete = (id) => {
+  //   const newBlogs = blogs.filter((blog) => blog.id !== id);
+  //   setBlogs(newBlogs);
+  // };
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-      });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          // console.log(res);
+          if (!res.ok) {
+            throw Error("could not fetch the data for that resource");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setError(null);
+          setBlogs(data);
+          setIsPending(false);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 1000);
   });
 
   return (
@@ -25,19 +39,22 @@ const Home = () => {
         <h2>Home Page</h2>
         {/* <button className="button">New Blog</button> */}
       </div>
-
+      {error && <div>{error}</div>}
+      {isPending && <div className="">Loading the Data......</div>}
       {blogs && (
         <BlogList
           blogs={blogs}
           title={"Newest Blogs"}
-          handleDelete={handleDelete}
+          // handleDelete={handleDelete}
         />
       )}
-      {blogs && <BlogList
-        blogs={blogs.filter((blog) => blog.author === "Yasser")}
-        title={"Yasser Blogs"}
-        handleDelete={handleDelete}
-      />}
+      {blogs && (
+        <BlogList
+          blogs={blogs.filter((blog) => blog.author === "Yasser")}
+          title={"Yasser Blogs"}
+          // handleDelete={handleDelete}
+        />
+      )}
     </div>
   );
 };
